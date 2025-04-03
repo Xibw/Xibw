d7604f82cc04e3b72f44fad6cf945fd11c76755a
appipinopi <appipinopi@gmail.com>
2025-04-03 11:51:49 +0900

    Update main.py
    Signed-off-by: appipinopi <appipinopi@gmail.com>

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+import asyncio
+from src.discord_bot import start_discord_bot
 from src.api_check import start_account_check
-from threading import Thread
 
 if __name__ == "__main__":
-    # Xアカウントのチェックを別スレッドで開始
-    account_check_thread = Thread(target=start_account_check)
-    account_check_thread.start()
+    loop = asyncio.get_event_loop()
+    
+    # Xアカウントのチェックを非同期で開始
+    loop.create_task(start_account_check())
+    
+    # Discord Botを起動
+    start_discord_bot()
