bdb6cb7a67289ee0405c460bf153829cf598b816
appipinopi <appipinopi@gmail.com>
2025-04-04 10:16:46 +0900

    Update test.py
    Signed-off-by: appipinopi <appipinopi@gmail.com>

--- commands/test.py
+++ commands/test.py
@@ -1,12 +1,12 @@
+import discord
 from discord.ext import commands
 
-class TestCommand(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
+# /ibw test コマンドの処理
+@commands.command()
+async def test(ctx):
+    await ctx.send("This is test program")
 
-    @commands.command()
-    async def test(self, ctx):
-        await ctx.send("This is test program")
-
+# Botにコマンドを登録する関数
 def setup(bot):
-    bot.add_cog(TestCommand(bot))
+    bot.add_command(test)
+
